72d0fb2edb0ea2896e85c670dde81cdd26250f09
kadunurivamshi <kadunurivamshi@gmail.com>
2022-03-05 15:15:50 +0530

    done

---
+++ Phase-1/Assessment-Project/SearchFile/src/searchfile/SearchFile.java
@@ -0,0 +1,32 @@
+package searchfile;
+import java.io.File;
+import java.util.Scanner;
+public class SearchFile 
+{
+	public static void main(String[] args)
+	{
+		String path="D:\\Simpli_file\\";
+		File f=new File(path);
+		Scanner sc=new Scanner(System.in);
+		System.out.println("eneter a file to search");
+		String filenameserach=sc.next();
+		File filename[]=f.listFiles();
+		int flag=0;
+		for(File ff:filename) {
+			if(ff.getName().equals(filenameserach)){
+				flag=1;
+				break;
+			}
+			else {
+				flag=0;
+			}
+		}
+		if(flag==1) {
+			System.out.println("file is available");
+		}
+		else {
+			System.out.println("file is not available");
+		}
+		System.exit(0);
+	}
+}
---
+++ Phase-1/Assessment-Project/file_creation/src/file_creation/MainFile.java
@@ -0,0 +1,38 @@
+package file_creation;
+import java.io.IOException;
+import java.util.Scanner;
+import deletefile.DeleteFile;
+import searchfile.SearchFile;
+public class MainFile 
+{
+	public static void main(String[] args) throws IOException
+	{
+		int choice = -1;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("1.Insert");
+			System.out.println("2. Delete");
+			System.out.println("3. search");
+			System.out.println("4. Exit");
+
+			choice = sc.nextInt();
+
+			MainFile mf = new MainFile();
+			switch(choice) {
+			case 1: Create_File cf = new Create_File();
+			Create_File.main(null);
+			break;
+
+			case 2: DeleteFile df = new DeleteFile();
+			df.main(null);
+			break;
+
+			case 3:	SearchFile sf = new SearchFile();
+			sf.main(null);
+			break;  
+			case 4: System.exit(0);
+			break;
+			}
+		}while(choice!=0);
+	}
+}
