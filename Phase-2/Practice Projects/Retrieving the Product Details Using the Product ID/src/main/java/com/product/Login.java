aa8465a74ec7128e96c68872a826d73f1e0010f8
kadunurivamshi <kadunurivamshi@gmail.com>
2022-03-21 21:38:31 +0530

    done

--- Phase-2/Practice Projects/Retrieving the Product Details Using the Product ID/src/main/java/com/product/Login.java
+++ Phase-2/Practice Projects/Retrieving the Product Details Using the Product ID/src/main/java/com/product/Login.java
@@ -14,35 +14,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/login")
+@WebServlet("/Login1")
 public class Login extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String url="jdbc:mysql://localhost:3306/product_database";
+		String url="jdbc:mysql://localhost:3306/student_details";
 		String uname="root";
-		String pass="Vamshi@100";
+		String pass="vamshi@100";
+		
 		
 		response.setContentType("text/html");
 		
-		int pId = Integer.parseInt(request.getParameter("p_ID"));
+		String pId = request.getParameter("p_ID");
 	
 		PrintWriter out = response.getWriter();
 		
-		String query="select * from product where productID=?";
+		String query="select * from product where p_id=?";
 		out.print("<h1>Displaying the Product Details...</h1>");
 		out.print("<table border='1'><tr><th>Product Id</th><th>Product Name</th><th>Product Price</th></tr>");
 		
 		try {
-	      Class.forName("com.mysql.jdbc.Driver");
+	      Class.forName("com.mysql.cj.jdbc.Driver");
 	      Connection dbCon = DriverManager.getConnection(url, uname, pass);
 	      PreparedStatement st=  dbCon.prepareStatement(query);
 	      
-	      st.setInt(1, pId);
+	      st.setString(1, pId);
 	      
 	      ResultSet rs =st.executeQuery();
 	      
 	      while(rs.next()) {
+	    	  
 	    	  out.print("<tr><td>");
 	    	  out.println(rs.getInt(1));
 	    	  out.print("</td>");
@@ -50,18 +52,23 @@ public class Login extends HttpServlet{
 	    	  out.print(rs.getString(2));
 	    	  out.print("</td>");
 	    	  out.print("<td>");
-	    	  out.print(rs.getString(3));
+	    	  out.print(rs.getInt(3));
 	    	  out.print("</td>");
 	    	  out.print("</tr>");
+	    
+	    	  
 			}
 	      
 		}
 		catch(Exception e){
 			
 			System.out.println("Some Issue : "+ e.getMessage());
+			
+			
 		}
 		
 		out.print("</table>");
+		
 	}
 
 }
